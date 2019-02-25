a710f9dfbdcd660176cf83599c295f5b8d535434
Ferran Llamas <ferran@onna.com>
2019-02-25 11:04:53 +0100

    Changelog and version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_amqp',
-    version='2.3.0',
+    version='2.3.1',
     description='Integrate amqp into guillotina',
     long_description=README,
     install_requires=[
